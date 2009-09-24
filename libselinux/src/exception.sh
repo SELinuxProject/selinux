95d8143b35913fc34bb6c92f7c36f2e155c53049
Joshua Brindle <method@manicmethod.com>
2009-09-24 13:46:12 -0400

    This updates commit 66d07600075d53735197520e4a5bbe6796a89d25
    This seems to work better on my system (aux-info on temp.c didn't do anything)

--- libselinux/src/exception.sh
+++ libselinux/src/exception.sh
@@ -15,7 +15,6 @@ echo "
 ;;
 esac
 }
-echo '#include "../include/selinux/selinux.h"' > temp.c
-gcc -c temp.c -aux-info temp.aux 
-for i in `awk '/..\/include\/selinux\/selinux.h.*extern int/ { print $6 }' temp.aux`; do except $i ; done 
-rm -f temp.c temp.aux temp.o
+gcc -x c -c - -aux-info temp.aux < ../include/selinux/selinux.h
+for i in `awk '/<stdin>.*extern int/ { print $6 }' temp.aux`; do except $i ; done 
+rm -f -- temp.aux -.o
