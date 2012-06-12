ac5f5645b6f285a66ceceb5625e05fbbe3ac1329
Eric Paris <eparis@redhat.com>
2012-06-12 11:01:11 -0400

    libselinux: stop messages when SELinux disabled
    If SELinux is disabled we should send any messages.  We shouldn't do
    anything.  Just return.
    
    Signed-off-by: Eric Paris <eparis@redhat.com>

--- libselinux/src/callbacks.c
+++ libselinux/src/callbacks.c
@@ -16,6 +16,7 @@ default_selinux_log(int type __attribute__((unused)), const char *fmt, ...)
 {
 	int rc;
 	va_list ap;
+	if (is_selinux_enabled() == 0) return 0;
 	va_start(ap, fmt);
 	rc = vfprintf(stderr, fmt, ap);
 	va_end(ap);
