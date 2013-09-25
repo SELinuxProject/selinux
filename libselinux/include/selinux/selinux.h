32e76a314ed30b9be3f860e1686a9271d1c2620d
Sven Vermeulen <sven.vermeulen@siphos.be>
2013-09-25 17:52:02 +0200

    Current stable policycoreutils requires selinux_current_policy_path
    Without this fix, stolen from Fedora's patchset, recent policycoreutils
    just fails.
    
    See http://marc.info/?t=136926404600001&r=1&w=2 and
    see http://marc.info/?l=selinux&m=136692033821285&w=2
    
    Signed-off-by: Sven Vermeulen <sven.vermeulen@siphos.be>

--- libselinux/include/selinux/selinux.h
+++ libselinux/include/selinux/selinux.h
@@ -498,6 +498,7 @@ extern const char *selinux_policy_root(void);
 
 /* These functions return the paths to specific files under the 
    policy root directory. */
+extern const char *selinux_current_policy_path(void);
 extern const char *selinux_binary_policy_path(void);
 extern const char *selinux_failsafe_context_path(void);
 extern const char *selinux_removable_context_path(void);
--- libselinux/src/selinux_config.c
+++ libselinux/src/selinux_config.c
@@ -8,6 +8,7 @@
 #include <limits.h>
 #include <unistd.h>
 #include <pthread.h>
+#include "policy.h"
 #include "selinux_internal.h"
 #include "get_default_type_internal.h"
 
@@ -302,6 +303,28 @@ const char *selinux_binary_policy_path(void)
 }
 
 hidden_def(selinux_binary_policy_path)
+
+const char *selinux_current_policy_path(void)
+{
+	int rc = 0;
+	int vers = 0;
+	static char policy_path[PATH_MAX];
+
+	snprintf(policy_path, sizeof(policy_path), "%s/policy", selinux_mnt);
+	if (access(policy_path, F_OK) == 0) {
+		vers = security_policyvers();
+		do {
+			/* Check prior versions to see if old policy is available */
+			snprintf(policy_path, sizeof(policy_path), "%s.%d",
+				selinux_binary_policy_path(), vers);
+		} while ((rc = access(policy_path, F_OK)) && --vers > 0);
+
+		if (rc) return NULL;
+	}
+	return policy_path;
+}
+
+hidden_def(selinux_current_policy_path)
 
 const char *selinux_file_context_path(void)
 {
