cde05f3b6115730fb1e123dd057edc89d80e398a
Dan Walsh <dwalsh@redhat.com>
2013-10-28 10:35:57 -0400

    Have sepolgen return additional constraint information

--- sepolgen/src/sepolgen/policygen.py
+++ sepolgen/src/sepolgen/policygen.py
@@ -174,7 +174,8 @@ class PolicyGenerator:
             if av.type == audit2why.CONSTRAINT:
                 rule.comment += "\n#!!!! This avc is a constraint violation.  You would need to modify the attributes of either the source or target types to allow this access."
                 rule.comment += "\n#Constraint rule: "
-                for reason in av.data:
+                rule.comment += "\n\t" + av.data[0]
+                for reason in av.data[1:]:
                     rule.comment += "\n#\tPossible cause is the source %s and target %s are different." % reason
 
             try:
