28efbfd4ed1b57932d8a485197ddd29879d82998
Yuli Khodorkovskiy <ykhodorkovskiy@tresys.com>
2014-04-03 16:58:12 -0400

    policycoreutils: add semodule option to set store root path
    Add a new -S option to semodule. This option overrides store_root
    in semanage.conf and sets the SELinux store's root path. If neither -S,
    nor store_root are specified in semanage.conf, then the default
    location is used.
    
    Signed-off-by: Yuli Khodorkovskiy <ykhodorkovskiy@tresys.com>

--- policycoreutils/semodule/semodule.c
+++ policycoreutils/semodule/semodule.c
@@ -52,6 +52,7 @@ static uint16_t priority;
 
 static semanage_handle_t *sh = NULL;
 static char *store;
+static char *store_root;
 
 extern char *optarg;
 extern int optind;
@@ -78,6 +79,20 @@ static void set_store(char *storename)
 	 * should support an address for a remote connection */
 
 	if ((store = strdup(storename)) == NULL) {
+		fprintf(stderr, "Out of memory!\n");
+		goto bad;
+	}
+
+	return;
+
+      bad:
+	cleanup();
+	exit(1);
+}
+
+static void set_store_root(char *path)
+{
+	if ((store_root = strdup(path)) == NULL) {
 		fprintf(stderr, "Out of memory!\n");
 		goto bad;
 	}
@@ -124,6 +139,7 @@ static void usage(char *progname)
 	printf("  -P,--preserve_tunables	Preserve tunables in policy\n");
 	printf("  -C,--ignore-module-cache	Rebuild CIL modules compiled from HLL files\n");
 	printf("  -p,--path        use an alternate path for the policy root\n");
+	printf("  -S,--store-path  use an alternate path for the policy store root\n");
 }
 
 /* Sets the global mode variable to new_mode, but only if no other
@@ -173,6 +189,7 @@ static void parse_command_line(int argc, char **argv)
 		{"enable", required_argument, NULL, 'e'},
 		{"disable", required_argument, NULL, 'd'},
 		{"path", required_argument, NULL, 'p'},
+		{"store-path", required_argument, NULL, 'S'},
 		{NULL, 0, NULL, 0}
 	};
 	int i;
@@ -181,7 +198,7 @@ static void parse_command_line(int argc, char **argv)
 	no_reload = 0;
 	priority = 400;
 	while ((i =
-		getopt_long(argc, argv, "s:b:hi:l::vqr:u:RnNBDCPX:e:d:p:", opts,
+		getopt_long(argc, argv, "s:b:hi:l::vqr:u:RnNBDCPX:e:d:p:S:", opts,
 			    NULL)) != -1) {
 		switch (i) {
 		case 'b':
@@ -212,6 +229,9 @@ static void parse_command_line(int argc, char **argv)
 			break;
 		case 'p':
 			semanage_set_root(optarg);
+			break;
+		case 'S':
+			set_store_root(optarg);
 			break;
 		case 'R':
 			reload = 1;
@@ -315,6 +335,10 @@ int main(int argc, char *argv[])
 		 * option will need to be used later to specify a policy server 
 		 * location */
 		semanage_select_store(sh, store, SEMANAGE_CON_DIRECT);
+	}
+
+	if (store_root) {
+		semanage_set_store_root(sh, store_root);
 	}
 
 	/* create store if necessary, for bootstrapping */
