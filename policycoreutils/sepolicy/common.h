356a6e2566d84ada0e7bd46c05f884c47332efc6
Jason Zaman <jason@perfinion.com>
2016-08-05 02:33:57 +0800

    sepolicy: Add python3 PyInt and PyString defs
    Signed-off-by: James Carter <jwcart2@tycho.nsa.gov>

--- policycoreutils/sepolicy/common.h
+++ policycoreutils/sepolicy/common.h
@@ -11,6 +11,25 @@
 
 #define py_decref(x) { if (x) 	Py_DECREF(x); }
 
+#if PY_MAJOR_VERSION >= 3
+#	define PyIntObject                  PyLongObject
+#	define PyInt_Type                   PyLong_Type
+#	define PyInt_Check(op)              PyLong_Check(op)
+#	define PyInt_CheckExact(op)         PyLong_CheckExact(op)
+#	define PyInt_FromString             PyLong_FromString
+#	define PyInt_FromUnicode            PyLong_FromUnicode
+#	define PyInt_FromLong               PyLong_FromLong
+#	define PyInt_FromSize_t             PyLong_FromSize_t
+#	define PyInt_FromSsize_t            PyLong_FromSsize_t
+#	define PyInt_AsLong                 PyLong_AsLong
+#	define PyInt_AS_LONG                PyLong_AS_LONG
+#	define PyInt_AsSsize_t              PyLong_AsSsize_t
+#	define PyInt_AsUnsignedLongMask     PyLong_AsUnsignedLongMask
+#	define PyInt_AsUnsignedLongLongMask PyLong_AsUnsignedLongLongMask
+#	define PyString_FromString          PyUnicode_FromString
+#	define PyString_AsString            PyUnicode_AsUTF8
+#endif
+
 static int py_append_string(PyObject *list, const char* value)
 {
 	int rt;
