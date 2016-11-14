db17f12273adf7bdee4c7d86ac4d71cc8bd78d4f
Nicolas Iooss <nicolas.iooss@m4x.org>
2016-11-14 22:57:11 +0100

    libselinux,libsemanage: fall back to gcc in exception.sh
    clang does not support -aux-info option. When exception.sh is run with
    CC=clang, use gcc to build selinuxswig_python_exception.i and
    semanageswig_python_exception.i.
    
    This does not solve the issue of building libselinux and libsemanage
    Python wrappers on a system without gcc. However parsing the result of
    "gcc -aux-info" is easier than parsing the header files so stay with
    this command at least for now.
    
    Signed-off-by: Nicolas Iooss <nicolas.iooss@m4x.org>

--- libselinux/src/exception.sh
+++ libselinux/src/exception.sh
@@ -15,6 +15,10 @@ echo "
 ;;
 esac
 }
-${CC:-gcc} -x c -c -I../include - -aux-info temp.aux < ../include/selinux/selinux.h
+if ! ${CC:-gcc} -x c -c -I../include - -aux-info temp.aux < ../include/selinux/selinux.h
+then
+    # clang does not support -aux-info so fall back to gcc
+    gcc -x c -c -I../include - -aux-info temp.aux < ../include/selinux/selinux.h
+fi
 for i in `awk '/<stdin>.*extern int/ { print $6 }' temp.aux`; do except $i ; done 
 rm -f -- temp.aux -.o
