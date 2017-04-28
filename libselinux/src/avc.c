9cba8f61515b07982a7ce8bf6359ab9278de9228
Richard Haines <richard_c_haines@btinternet.com>
2017-04-28 14:05:16 +0100

    libselinux: Add permissive= entry to avc audit log
    Add audit log entry to specify whether the decision was made in
    permissive mode/permissive domain or enforcing mode.
    
    Signed-off-by: Richard Haines <richard_c_haines@btinternet.com>

--- libselinux/src/avc.c
+++ libselinux/src/avc.c
@@ -723,6 +723,10 @@ void avc_audit(security_id_t ssid, security_id_t tsid,
 
 	log_append(avc_audit_buf, " ");
 	avc_dump_query(ssid, tsid, tclass);
+
+	if (denied)
+		log_append(avc_audit_buf, " permissive=%u", result ? 0 : 1);
+
 	log_append(avc_audit_buf, "\n");
 	avc_log(SELINUX_AVC, "%s", avc_audit_buf);
 
