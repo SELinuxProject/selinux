2cf6167c5d14d4559105990916d56c22d53065c4
Nicolas Iooss <nicolas.iooss@m4x.org>
2018-04-13 22:34:20 +0200

    libsepol: do not dereference NULL if stack_init fails
    In cond_expr_to_cil(), when stack_init() fails to allocate a stack, the
    function calls stack_pop() with stack = NULL. Then stack_pop()
    dereferences the pointer ("if (stack->pos == -1) {"), which is NULL.
    
    Fix this by moving the stack cleaning loop in a "if (stack != NULL)"
    block.
    
    This issue is reported by clang's static analyzer with the following
    message:
    
        module_to_cil.c:463:6: warning: Access to field 'pos' results in a
        dereference of a null pointer (loaded from variable 'stack')
            if (stack->pos == -1) {
                ^~~~~~~~~~
    
    Signed-off-by: Nicolas Iooss <nicolas.iooss@m4x.org>

--- libsepol/src/module_to_cil.c
+++ libsepol/src/module_to_cil.c
@@ -1917,10 +1917,12 @@ exit:
 	free(new_val);
 	free(val1);
 	free(val2);
-	while ((val1 = stack_pop(stack)) != NULL) {
-		free(val1);
+	if (stack != NULL) {
+		while ((val1 = stack_pop(stack)) != NULL) {
+			free(val1);
+		}
+		stack_destroy(&stack);
 	}
-	stack_destroy(&stack);
 
 	return rc;
 }
