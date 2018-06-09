49a49639118e9ea4aed644058f42d48cf65a9402
Nicolas Iooss <nicolas.iooss@m4x.org>
2018-06-09 21:30:29 +0200

    libsepol/tests: read_binary_policy() does not use f.handle
    f.handle is never set in read_binary_policy() so there is no need to
    call sepol_handle_destroy() on it. Moreover clang's static analyzer
    warns about an uninitialized argument value in the first call.
    
    Signed-off-by: Nicolas Iooss <nicolas.iooss@m4x.org>

--- libsepol/tests/test-downgrade.c
+++ libsepol/tests/test-downgrade.c
@@ -210,7 +210,6 @@ int read_binary_policy(const char *path, policydb_t *p)
 	if ((in_fp = fopen(path, "rb")) == NULL) {
 		fprintf(stderr, "Unable to open %s: %s\n", path,
 			strerror(errno));
-		sepol_handle_destroy(f.handle);
 		return -1;
 	}
 
@@ -220,7 +219,6 @@ int read_binary_policy(const char *path, policydb_t *p)
 	f.fp = in_fp;
 	rc = policydb_read(p, &f, 0);
 
-	sepol_handle_destroy(f.handle);
 	fclose(in_fp);
 	return rc;
 }
