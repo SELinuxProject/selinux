a8feb8e3686f9e450544ba22f96370fa276962c0
Nick Kralevich via Selinux <selinux@tycho.nsa.gov>
2018-09-24 11:10:51 -0700

    whitespace and spelling cleanup
    Signed-off-by: Nick Kralevich <nnk@google.com>

--- secilc/secilc.c
+++ secilc/secilc.c
@@ -1,16 +1,16 @@
 /*
  * Copyright 2011 Tresys Technology, LLC. All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  *    1. Redistributions of source code must retain the above copyright notice,
  *       this list of conditions and the following disclaimer.
- * 
+ *
  *    2. Redistributions in binary form must reproduce the above copyright notice,
  *       this list of conditions and the following disclaimer in the documentation
  *       and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY TRESYS TECHNOLOGY, LLC ``AS IS'' AND ANY EXPRESS
  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
@@ -21,7 +21,7 @@
  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * The views and conclusions contained in the software and documentation are those
  * of the authors and should not be interpreted as representing official policies,
  * either expressed or implied, of Tresys Technology, LLC.
@@ -259,7 +259,7 @@ int main(int argc, char *argv[])
 			fprintf(stderr, "Could not stat file: %s\n", argv[i]);
 			goto exit;
 		}
-		file_size = filedata.st_size;	
+		file_size = filedata.st_size;
 
 		buffer = malloc(file_size);
 		rc = fread(buffer, file_size, 1, file);
@@ -347,7 +347,7 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Failed to open file_contexts file\n");
 		goto exit;
 	}
-	
+
 	if (fwrite(fc_buf, sizeof(char), fc_size, file_contexts) != fc_size) {
 		fprintf(stderr, "Failed to write file_contexts file\n");
 		goto exit;
