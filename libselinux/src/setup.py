2efa06857575e4118e91ca250b6b92da68b130d5
Petr Lautrbach <plautrba@redhat.com>
2019-06-07 17:35:44 +0200

    libselinux: Use Python distutils to install SELinux python bindings
    Follow officially documented way how to build C extension modules using
    distutils - https://docs.python.org/3.8/extending/building.html#building
    
    Fixes:
    
    - selinux python module fails to load when it's built using SWIG-4.0:
    
    >>> import selinux
    Traceback (most recent call last):
      File "<stdin>", line 1, in <module>
      File "/usr/lib64/python3.7/site-packages/selinux/__init__.py", line 13, in <module>
        from . import _selinux
    ImportError: cannot import name '_selinux' from 'selinux' (/usr/lib64/python3.7/site-packages/selinux/__init__.py)
    
    SWIG-4.0 changed (again?) its behavior so that it uses: from . import _selinux
    which looks for _selinux module in the same directory as where __init__.py is -
    $(PYLIBDIR)/site-packages/selinux. But _selinux module is installed into
    $(PYLIBDIR)/site-packages/ since a9604c30a5e2f ("libselinux: Change the location
    of _selinux.so").
    
    - audit2why python module fails to build with Python 3.8
    
    cc -O2 -g -pipe -Wall -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2 -Wp,-D_GLIBCXX_ASSERTIONS -fexceptions -fstack-protector-strong -grecord-gcc-switches -specs=/usr/lib/rpm/redhat/redhat-hardened-cc1 -specs=/usr/lib/rpm/redhat/redhat-annobin-cc1 -m64 -mtune=generic -fasynchronous-unwind-tables -fstack-clash-protection -fcf-protection -DOVERRIDE_GETTID=0 -I../include -D_GNU_SOURCE -DDISABLE_RPM -DNO_ANDROID_BACKEND -DUSE_PCRE2 -DPCRE2_CODE_UNIT_WIDTH=8  -Wl,-z,relro -Wl,--as-needed  -Wl,-z,now -specs=/usr/lib/rpm/redhat/redhat-hardened-ld -L. -shared -o python-3.8audit2why.so python-3.8audit2why.lo -lselinux -l:libsepol.a  -Wl,-soname,audit2why.so,--version-script=audit2why.map,-z,defs
    /usr/bin/ld: python-3.8audit2why.lo: in function `finish':
    /builddir/build/BUILD/libselinux-2.9/src/audit2why.c:166: undefined reference to `PyArg_ParseTuple'
    /usr/bin/ld: python-3.8audit2why.lo: in function `_Py_INCREF':
    /usr/include/python3.8/object.h:449: undefined reference to `_Py_NoneStruct'
    /usr/bin/ld: /usr/include/python3.8/object.h:449: undefined reference to `_Py_NoneStruct'
    /usr/bin/ld: python-3.8audit2why.lo: in function `check_booleans':
    /builddir/build/BUILD/libselinux-2.9/src/audit2why.c:84: undefined reference to `PyExc_RuntimeError'
    ...
    
    It's related to the following Python change
    https://docs.python.org/dev/whatsnew/3.8.html#debug-build-uses-the-same-abi-as-release-build
    
    Python distutils adds correct link options automatically.
    
    - selinux python module doesn't provide any Python metadata
    
    When selinux python module was built manually, it didn't provide any metadata.
    distutils takes care about that so that selinux Python module is visible for
    pip:
    
    $ pip3 list | grep selinux
    selinux              2.9
    
    Signed-off-by: Petr Lautrbach <plautrba@redhat.com>

---
+++ libselinux/src/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+from distutils.core import Extension, setup
+
+setup(
+    name="selinux",
+    version="2.9",
+    description="SELinux python 3 bindings",
+    author="SELinux Project",
+    author_email="selinux@vger.kernel.org",
+    ext_modules=[
+        Extension('selinux._selinux',
+                  sources=['selinuxswig_python.i'],
+                  include_dirs=['../include'],
+                  library_dirs=['.'],
+                  libraries=['selinux']),
+        Extension('selinux.audit2why',
+                  sources=['audit2why.c'],
+                  include_dirs=['../include'],
+                  library_dirs=['.'],
+                  libraries=['selinux'],
+                  extra_link_args=['-l:libsepol.a', '-Wl,--version-script=audit2why.map'])
+    ],
+)
