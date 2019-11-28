dca7ce8195d241bf44eb4b158ef2e5d6e3b9963a
Petr Lautrbach <plautrba@redhat.com>
2019-11-28 13:45:08 +0100

    Update VERSIONs to 3.0 for release.
    Signed-off-by: Petr Lautrbach <plautrba@redhat.com>

--- python/sepolicy/setup.py
+++ python/sepolicy/setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 
 setup(
     name="sepolicy",
-    version="3.0-rc2",
+    version="3.0",
     description="Python SELinux Policy Analyses bindings",
     author="Daniel Walsh",
     author_email="dwalsh@redhat.com",
