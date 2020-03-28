047ce184e14bb2027de11431b68dcc4db934fe4d
Topi Miettinen <toiwoton@gmail.com>
2020-03-28 23:21:47 +0200

    libselinux: mount selinuxfs nodev,noexec,nosuid
    Mount selinuxfs with mount flags nodev,noexec and nosuid. It's not
    likely that this has any effect, but it's visually more pleasing.
    
    Signed-off-by: Topi Miettinen <toiwoton@gmail.com>

--- libselinux/src/load_policy.c
+++ libselinux/src/load_policy.c
@@ -279,7 +279,7 @@ int selinux_init_load_policy(int *enforce)
 	const char *mntpoint = NULL;
 	/* First make sure /sys is mounted */
 	if (mount("sysfs", "/sys", "sysfs", 0, 0) == 0 || errno == EBUSY) {
-		if (mount(SELINUXFS, SELINUXMNT, SELINUXFS, 0, 0) == 0 || errno == EBUSY) {
+		if (mount(SELINUXFS, SELINUXMNT, SELINUXFS, MS_NODEV | MS_NOEXEC | MS_NOSUID, 0) == 0 || errno == EBUSY) {
 			mntpoint = SELINUXMNT;
 		} else {
 			/* check old mountpoint */
