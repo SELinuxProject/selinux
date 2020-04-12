0bcaba30d77a2a0f9958d2269d331b250a19850c
Nicolas Iooss <nicolas.iooss@m4x.org>
2020-04-12 10:09:59 +0200

    libselinux: add missing glue code to grab errno in Python bindings
    The Python bindings for libselinux expose functions such as
    avc_has_perm(), get_ordered_context_list(), etc. When these functions
    encounter an error, they set errno accordingly and return a negative
    value. In order to get the value of errno from Python code, it needs to
    be "forwarded" in a way. This is achieved by glue code in
    selinuxswig_python_exception.i, which implement raising an OSError
    exception from the value of errno.
    
    selinuxswig_python_exception.i was only generating glue code from
    functions declared in selinux.h and not in other headers. Add other
    headers.
    
    selinuxswig_python_exception.i is generated by "bash exception.sh". Mark
    the fact that exception.sh is a Bash script by adding a shebang. This
    makes "shellcheck" not warn about the Bash array which is used to list
    header files.
    
    Signed-off-by: Nicolas Iooss <nicolas.iooss@m4x.org>
    Acked-by: William Roberts <william.c.roberts@intel.com>

--- libselinux/src/exception.sh
+++ libselinux/src/exception.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 function except() {
 case $1 in
     selinux_file_context_cmp) # ignore
@@ -15,10 +17,22 @@ echo "
 ;;
 esac
 }
-if ! ${CC:-gcc} -x c -c -I../include -o temp.o - -aux-info temp.aux < ../include/selinux/selinux.h
+
+# Make sure that selinux.h is included first in order not to depend on the order
+# in which "#include <selinux/selinux.h>" appears in other files.
+FILE_LIST=(
+    ../include/selinux/selinux.h
+    ../include/selinux/avc.h
+    ../include/selinux/context.h
+    ../include/selinux/get_context_list.h
+    ../include/selinux/get_default_type.h
+    ../include/selinux/label.h
+    ../include/selinux/restorecon.h
+)
+if ! cat "${FILE_LIST[@]}" | ${CC:-gcc} -x c -c -I../include -o temp.o - -aux-info temp.aux
 then
     # clang does not support -aux-info so fall back to gcc
-    gcc -x c -c -I../include -o temp.o - -aux-info temp.aux < ../include/selinux/selinux.h
+    cat "${FILE_LIST[@]}" | gcc -x c -c -I../include -o temp.o - -aux-info temp.aux
 fi
 for i in `awk '/<stdin>.*extern int/ { print $6 }' temp.aux`; do except $i ; done 
 rm -f -- temp.aux temp.o
