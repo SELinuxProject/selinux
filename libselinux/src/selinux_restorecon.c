b0034dec56b54b1b051632b4c1cee9f1a4d5213a
yanjinjq <66478206+yanjinjq@users.noreply.github.com>
2020-06-16 18:02:11 +0800

    fix NULL pointer use in selinux_restorecon_set_sehandle

--- libselinux/src/selinux_restorecon.c
+++ libselinux/src/selinux_restorecon.c
@@ -1153,9 +1153,11 @@ void selinux_restorecon_set_sehandle(struct selabel_handle *hndl)
 	size_t num_specfiles, fc_digest_len;
 
 	fc_sehandle = (struct selabel_handle *) hndl;
+	if (!fc_sehandle)
+		return;
 
 	/* Check if digest requested in selabel_open(3), if so use it. */
-	if (fc_sehandle == NULL || selabel_digest(fc_sehandle, &fc_digest, &fc_digest_len,
+	if (selabel_digest(fc_sehandle, &fc_digest, &fc_digest_len,
 				   &specfiles, &num_specfiles) < 0)
 		selabel_no_digest = true;
 	else
