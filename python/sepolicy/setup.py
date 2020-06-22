7df27b78e9eecbe65a57cdfefb9e51f547231b20
Petr Lautrbach <plautrba@redhat.com>
2020-06-22 16:49:26 +0200

    Update VERSIONs and Python bindings version to 3.1 for release
    Signed-off-by: Petr Lautrbach <plautrba@redhat.com>

--- python/sepolicy/setup.py
+++ python/sepolicy/setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 
 setup(
     name="sepolicy",
-    version="3.0",
+    version="3.1",
     description="Python SELinux Policy Analyses bindings",
     author="Daniel Walsh",
     author_email="dwalsh@redhat.com",
