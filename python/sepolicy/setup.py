2c7c4a84c38da318a99a28990e293dde53f79bed
Petr Lautrbach <plautrba@redhat.com>
2021-02-03 10:09:58 +0100

    Update VERSIONs to 3.2-rc2 for release.
    Signed-off-by: Petr Lautrbach <plautrba@redhat.com>

--- python/sepolicy/setup.py
+++ python/sepolicy/setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 
 setup(
     name="sepolicy",
-    version="3.2-rc1",
+    version="3.2-rc2",
     description="Python SELinux Policy Analyses bindings",
     author="Daniel Walsh",
     author_email="dwalsh@redhat.com",
