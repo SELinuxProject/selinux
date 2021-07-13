1a6af29939e463dde9f7019437d6862c410efbd3
Evgeny Vereshchagin <evvers@ya.ru>
2021-07-13 14:06:32 +0000

    oss-fuzz: make shellcheck happy
    Signed-off-by: Evgeny Vereshchagin <evvers@ya.ru>

--- scripts/oss-fuzz.sh
+++ scripts/oss-fuzz.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-export DESTDIR=$(pwd)/DESTDIR
+export DESTDIR=${DESTDIR:-$(pwd)/DESTDIR}
 
 SANITIZER=${SANITIZER:-address}
 flags="-O1 -fno-omit-frame-pointer -gline-tables-only -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION -fsanitize=$SANITIZER -fsanitize=fuzzer-no-link"
@@ -16,13 +16,18 @@ export CXXFLAGS=${CXXFLAGS:-$flags}
 export LDFLAGS="${LDFLAGS:-} $CFLAGS"
 
 export OUT=${OUT:-$(pwd)/out}
-mkdir -p $OUT
+mkdir -p "$OUT"
 
 export LIB_FUZZING_ENGINE=${LIB_FUZZING_ENGINE:--fsanitize=fuzzer}
 
-find -name Makefile | xargs sed -i 's/,-z,defs//'
-make V=1 -j$(nproc) install
+find . -name Makefile -print0 | xargs -0 sed -i 's/,-z,defs//'
+make V=1 -j"$(nproc)" install
 
-$CC $CFLAGS -I$DESTDIR/usr/include -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -c -o secilc-fuzzer.o libsepol/fuzz/secilc-fuzzer.c
-$CXX $CXXFLAGS $LIB_FUZZING_ENGINE secilc-fuzzer.o $DESTDIR/usr/lib/libsepol.a -o $OUT/secilc-fuzzer
-zip -r $OUT/secilc-fuzzer_seed_corpus.zip secilc/test
+# CFLAGS, CXXFLAGS and LIB_FUZZING_ENGINE have to be split to be accepted by
+# the compiler/linker so they shouldn't be quoted
+# shellcheck disable=SC2086
+$CC $CFLAGS -I"$DESTDIR/usr/include" -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -c -o secilc-fuzzer.o libsepol/fuzz/secilc-fuzzer.c
+# shellcheck disable=SC2086
+$CXX $CXXFLAGS $LIB_FUZZING_ENGINE secilc-fuzzer.o "$DESTDIR/usr/lib/libsepol.a" -o "$OUT/secilc-fuzzer"
+
+zip -r "$OUT/secilc-fuzzer_seed_corpus.zip" secilc/test
