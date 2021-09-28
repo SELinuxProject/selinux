e32809155b72c61c6027ffddb320cea821df28e1
Christian Göttsche <cgzones@googlemail.com>
2021-09-28 17:46:17 +0200

    checkpolicy: print reason of fopen failure
    Print the reason why opening a source policy file failed, e.g:
    
        checkpolicy:  unable to open policy.conf:  No such file or directory
    
    Signed-off-by: Christian Göttsche <cgzones@googlemail.com>

--- checkpolicy/parse_util.c
+++ checkpolicy/parse_util.c
@@ -36,7 +36,7 @@ int read_source_policy(policydb_t * p, const char *file, const char *progname)
 {
 	yyin = fopen(file, "r");
 	if (!yyin) {
-		fprintf(stderr, "%s:  unable to open %s\n", progname, file);
+		fprintf(stderr, "%s:  unable to open %s:  %s\n", progname, file, strerror(errno));
 		return -1;
 	}
 	set_source_file(file);
