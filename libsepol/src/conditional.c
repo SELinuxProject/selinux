dfc652f01ea22e367c39d132873735473608f8fa
James Carter <jwcart2@gmail.com>
2022-03-14 14:17:11 -0400

    libsepol: Use calloc when initializing bool_val_to_struct array
    Use calloc() instead of mallocarray() so that everything is
    initialized to zero to prevent the use of unitialized memory when
    validating malformed binary policies.
    
    Found by oss-fuzz (#45493)
    
    Signed-off-by: James Carter <jwcart2@gmail.com>

--- libsepol/src/conditional.c
+++ libsepol/src/conditional.c
@@ -522,7 +522,7 @@ int cond_init_bool_indexes(policydb_t * p)
 	if (p->bool_val_to_struct)
 		free(p->bool_val_to_struct);
 	p->bool_val_to_struct = (cond_bool_datum_t **)
-	    mallocarray(p->p_bools.nprim, sizeof(cond_bool_datum_t *));
+	    calloc(p->p_bools.nprim, sizeof(cond_bool_datum_t *));
 	if (!p->bool_val_to_struct)
 		return -1;
 	return 0;
