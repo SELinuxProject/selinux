7d5a89314be88b3085cfdf6083837045a5e44aa9
Christian Göttsche <cgzones@googlemail.com>
2022-05-10 20:20:38 +0200

    libselinux: introduce strlcpy
    To copy string safely, by always NULL-terminating them, and provide an
    easy way to check for truncation introduce the nonstandard function
    strlcpy(3).  Use the system implementation if available.
    
    Signed-off-by: Christian Göttsche <cgzones@googlemail.com>

---
+++ libselinux/src/selinux_internal.c
@@ -0,0 +1,18 @@
+#include "selinux_internal.h"
+
+#include <string.h>
+
+
+#ifndef HAVE_STRLCPY
+size_t strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t ret = strlen(src);
+
+	if (size) {
+		size_t len = (ret >= size) ? size - 1 : ret;
+		memcpy(dest, src, len);
+		dest[len] = '\0';
+	}
+	return ret;
+}
+#endif /* HAVE_STRLCPY */
--- libselinux/src/selinux_internal.h
+++ libselinux/src/selinux_internal.h
@@ -94,4 +94,8 @@ extern int selinux_page_size ;
 
 extern int has_selinux_config ;
 
+#ifndef HAVE_STRLCPY
+size_t strlcpy(char *dest, const char *src, size_t size);
+#endif
+
 #endif /* SELINUX_INTERNAL_H_ */
