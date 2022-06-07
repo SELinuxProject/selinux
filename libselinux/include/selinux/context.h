a6c572b1af14ae53b692c092fdc8838fe56ab3b3
Christian Göttsche <cgzones@googlemail.com>
2022-06-07 19:16:47 +0200

    libselinux: name parameters in context.h
    Signed-off-by: Christian Göttsche <cgzones@googlemail.com>
    Acked-by: James Carter <jwcart2@gmail.com>

--- libselinux/include/selinux/context.h
+++ libselinux/include/selinux/context.h
@@ -17,7 +17,7 @@ extern "C" {
 
 /* Return a new context initialized to a context string */
 
-	extern context_t context_new(const char *);
+	extern context_t context_new(const char *str);
 
 /* 
  * Return a pointer to the string value of the context_t
@@ -25,24 +25,24 @@ extern "C" {
  * for the same context_t*
  */
 
-	extern const char *context_str(context_t);
+	extern const char *context_str(context_t con);
 
 /* Free the storage used by a context */
-	extern void context_free(context_t);
+	extern void context_free(context_t con);
 
 /* Get a pointer to the string value of a context component */
 
-	extern const char *context_type_get(context_t);
-	extern const char *context_range_get(context_t);
-	extern const char *context_role_get(context_t);
-	extern const char *context_user_get(context_t);
+	extern const char *context_type_get(context_t con);
+	extern const char *context_range_get(context_t con);
+	extern const char *context_role_get(context_t con);
+	extern const char *context_user_get(context_t con);
 
 /* Set a context component.  Returns nonzero if unsuccessful */
 
-	extern int context_type_set(context_t, const char *);
-	extern int context_range_set(context_t, const char *);
-	extern int context_role_set(context_t, const char *);
-	extern int context_user_set(context_t, const char *);
+	extern int context_type_set(context_t con, const char *type);
+	extern int context_range_set(context_t con, const char *range);
+	extern int context_role_set(context_t con, const char *role);
+	extern int context_user_set(context_t con, const char *user);
 
 #ifdef __cplusplus
 }
