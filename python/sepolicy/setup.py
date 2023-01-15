3ccea01c6926ea22aa9dbbacd32f441ecb0896bf
Jason Zaman <jason@perfinion.com>
2023-01-15 15:40:55 -0800

    Update VERSIONs to 3.5-rc2 for release.
    Signed-off-by: Jason Zaman <jason@perfinion.com>

--- python/sepolicy/setup.py
+++ python/sepolicy/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="sepolicy",
-    version="3.5-rc1",
+    version="3.5-rc2",
     description="Python SELinux Policy Analyses bindings",
     author="Daniel Walsh",
     author_email="dwalsh@redhat.com",
