f8a076f1cada747ea64fa392d4b952d690c003bb
Masatake YAMATO <yamato@redhat.com>
2023-06-08 10:52:40 +0900

    dispol: handle EOF in user interaction
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>
    Acked-by: James Carter <jwcart2@gmail.com>

--- checkpolicy/test/dispol.c
+++ checkpolicy/test/dispol.c
@@ -531,6 +531,8 @@ int main(int argc, char **argv)
 	for (;;) {
 		printf("\nCommand (\'m\' for menu):  ");
 		if (fgets(ans, sizeof(ans), stdin) == NULL) {
+			if (feof(stdin))
+				break;
 			fprintf(stderr, "fgets failed at line %d: %s\n", __LINE__,
 					strerror(errno));
 			continue;
