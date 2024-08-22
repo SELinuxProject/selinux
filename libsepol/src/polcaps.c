9c7c6e15a2c6bb8d53c41b8fa4890df6228fa83c
Thiébaud Weksteen <tweek@google.com>
2024-08-22 10:37:57 +1000

    libsepol: Add policy capability netlink_xperm
    This capability can be enabled to change the kernel's behaviour and use
    the extended permissions for netlink messages.
    
    Signed-off-by: Thiébaud Weksteen <tweek@google.com>
    Acked-by: Stephen Smalley <stephen.smalley.work@gmail.com>

--- libsepol/src/polcaps.c
+++ libsepol/src/polcaps.c
@@ -15,6 +15,7 @@ static const char * const polcap_names[POLICYDB_CAP_MAX + 1] = {
 	[POLICYDB_CAP_GENFS_SECLABEL_SYMLINKS]		= "genfs_seclabel_symlinks",
 	[POLICYDB_CAP_IOCTL_SKIP_CLOEXEC]		= "ioctl_skip_cloexec",
 	[POLICYDB_CAP_USERSPACE_INITIAL_CONTEXT]	= "userspace_initial_context",
+	[POLICYDB_CAP_NETLINK_XPERM]			= "netlink_xperm",
 };
 
 int sepol_polcap_getnum(const char *name)
