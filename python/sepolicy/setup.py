8e9157bbeea1899b7b8b257e7eaa71efef3fffed
Petr Lautrbach <lautrbach@redhat.com>
2025-03-05 19:59:06 +0100

    Update VERSIONs to 3.8.1 for release.
    Signed-off-by: Petr Lautrbach <lautrbach@redhat.com>

--- python/sepolicy/setup.py
+++ python/sepolicy/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="sepolicy",
-    version="3.8",
+    version="3.8.1",
     description="Python SELinux Policy Analyses bindings",
     author="Daniel Walsh",
     author_email="dwalsh@redhat.com",
