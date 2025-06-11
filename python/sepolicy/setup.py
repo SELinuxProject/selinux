054d7f0b4daef9194363a007c84f9bcbec598825
Petr Lautrbach <lautrbach@redhat.com>
2025-06-11 19:29:53 +0200

    Update VERSIONs to 3.9-rc1 for release.
    Signed-off-by: Petr Lautrbach <lautrbach@redhat.com>

--- python/sepolicy/setup.py
+++ python/sepolicy/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="sepolicy",
-    version="3.8.1",
+    version="3.9-rc1",
     description="Python SELinux Policy Analyses bindings",
     author="Daniel Walsh",
     author_email="dwalsh@redhat.com",
