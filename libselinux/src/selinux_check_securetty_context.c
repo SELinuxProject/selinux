7afef16158e3c67be8cebd2c1c5bcacd39979b6b
Rahul Sandhu <nvraxn@gmail.com>
2025-08-01 18:45:27 +0100

    libselinux: refactor selinux_check_securetty_context
    Invert the checks for fp and con.
    
    Signed-off-by: Rahul Sandhu <nvraxn@gmail.com>
    Acked-by: Stephen Smalley <stephen.smalley.work@gmail.com>

--- libselinux/src/selinux_check_securetty_context.c
+++ libselinux/src/selinux_check_securetty_context.c
@@ -8,45 +8,48 @@
 
 int selinux_check_securetty_context(const char * tty_context)
 {
+	FILE *fp = fopen(selinux_securetty_types_path(), "re");
+	if (!fp)
+		return -1;
+
+	context_t con = context_new(tty_context);
+	if (!con) {
+		fclose(fp);
+		return -1;
+	}
+
+	const char *type = context_type_get(con);
+
 	char *line = NULL;
 	char *start, *end = NULL;
 	size_t line_len = 0;
-	ssize_t len;
 	int found = -1;
-	FILE *fp;
-	fp = fopen(selinux_securetty_types_path(), "re");
-	if (fp) {
-		context_t con = context_new(tty_context);
-		if (con) {
-			const char *type = context_type_get(con);
-			while ((len = getline(&line, &line_len, fp)) != -1) {
+	ssize_t len;
+	while ((len = getline(&line, &line_len, fp)) != -1) {
+		if (line[len - 1] == '\n')
+			line[len - 1] = 0;
 
-				if (line[len - 1] == '\n')
-					line[len - 1] = 0;
+		/* Skip leading whitespace. */
+		start = line;
+		while (*start && isspace((unsigned char)*start))
+			start++;
+		if (!(*start))
+			continue;
 
-				/* Skip leading whitespace. */
-				start = line;
-				while (*start && isspace((unsigned char)*start))
-					start++;
-				if (!(*start))
-					continue;
-
-				end = start;
-				while (*end && !isspace((unsigned char)*end))
-					end++;
-				if (*end)
-					*end++ = 0;
-				if (!strcmp(type, start)) {
-					found = 0;
-					break;
-				}
-			}
-			free(line);
-			context_free(con);
+		end = start;
+		while (*end && !isspace((unsigned char)*end))
+			end++;
+		if (*end)
+			*end++ = 0;
+		if (!strcmp(type, start)) {
+			found = 0;
+			break;
 		}
-		fclose(fp);
 	}
+
+	free(line);
+	context_free(con);
+	fclose(fp);
 
 	return found;
 }
-
